6ad6c5a053ea1706ea3ae0a766c8c6a1c60bae63
Alex Nagel <slipshot21@gmail.com>
2020-09-18 22:42:54 -0500

    new

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import {Link} from 'react-router-dom'
 const Navbar = props => {
     return (
         <div className="nav_container">
-            <h2 className="heading title">Pledge Pals</h2>
+            <Link to="/" className="heading title">Pledge Pals</Link>
             <div className="authLinks">
                 <Link className="navButton loginButton" to = "/login">Login</Link>
                 <Link className="navButton registerButton" to = "/register">Sign Up</Link>
