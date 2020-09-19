9df095dae21f0ab504a5bd37371046979bac875d
Alex Nagel <slipshot21@gmail.com>
2020-09-18 21:51:45 -0500

    dumb navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import "./components/Login.jsx";
-import "./components/Home.jsx";
+import Login from "./components/Login.jsx";
+import Home from "./components/Home.jsx";
+import Navbar from "./components/navbar/Navbar"
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
     </div>
   );
 }
---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import "./styles/Navbar.css"
+
+const Navbar = props => {
+    return (
+        <div className="nav_container">
+            
+        </div>
+    )
+}
+
+Navbar.propTypes = {
+
+}
+
+export default Navbar
