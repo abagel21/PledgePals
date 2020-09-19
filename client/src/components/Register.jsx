8855c0396b2bee27d403382d1afd7f430fd458fa
Alex Nagel <slipshot21@gmail.com>
2020-09-18 23:00:50 -0500

    register

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -3,9 +3,29 @@ import PropTypes from 'prop-types'
 import "./styles/Auth.css"
 
 const Register = props => {
+    const onClick = (e) => {
+        e.preventDefault();
+    }
     return (
-        <div>
-            
+        <div className="registerWrapper">
+            <div className="formsContainer">
+                <form action="" className="registerForm">
+                    <h2 className="heading">Sign Up</h2>
+                    <div className="registerEmail">
+                        <input type="text" className="registerField username" required/>
+                        <label>Email Address</label>
+                    </div>
+                    <div className="registerPassword">
+                        <input type="password" className="registerField password" required/>
+                        <label>Password</label>
+                    </div>
+                    <div className="registerConfirm">
+                        <input type="password" className="registerField password" required/>
+                        <label>Confirm Password</label>
+                    </div>
+                    <input type="submit" className="registerSubmit" onClick = {e => onClick(e)}/>
+                </form>
+            </div>
         </div>
     )
 }
