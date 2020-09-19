53885dc45592d2b3c95d81edc6f42dc929a34396
Alex Nagel <slipshot21@gmail.com>
2020-09-18 21:59:21 -0500

    register

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import Login from "./components/Login.jsx";
 import Home from "./components/Home.jsx";
 import Navbar from "./components/navbar/Navbar"
