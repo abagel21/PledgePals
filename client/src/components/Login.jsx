8daf7ef7798d9af8ebf2f9440baaa3c004b30e5f
Alex Nagel <slipshot21@gmail.com>
2020-09-18 22:56:01 -0500

    login prototype

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,14 +3,23 @@ import PropTypes from 'prop-types'
 import "./styles/Auth.css"
 
 const Login = props => {
+    const onClick = (e) => {
+        e.preventDefault();
+    }
     return (
         <div className="loginWrapper">
             <div className="formsContainer">
                 <form action="" className="loginForm">
                     <h2 className="heading">Login</h2>
-                    <input type="text" className="loginField username" />
-                    <input type="password" className="loginField password"/>
-                    <input type="submit" className="loginSubmit"/>
+                    <div className="loginEmail">
+                        <input type="text" className="loginField username" required/>
+                        <label>Email Address</label>
+                    </div>
+                    <div className="loginPassword">
+                        <input type="password" className="loginField password" required/>
+                        <label>Password</label>
+                    </div>
+                    <input type="submit" className="loginSubmit" onClick = {e => onClick(e)}/>
                 </form>
             </div>
         </div>
