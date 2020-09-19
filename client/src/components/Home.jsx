5d0f2028464e0da0e450e6877e4861b9f0fe4573
platbullet <50148871+platbullet@users.noreply.github.com>
2020-09-18 23:12:17 -0500

    added image

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,9 +4,10 @@ import './styles/Home.css';
 
 const Home = props => {
     return (
-        <div className="homeAbsolute">
-            <div className="homeDiv">
+        <div className="home-page">
+            <div className="home-div">
                 <h1>Welcome to Pledge Pals!</h1>
+                <img src="../images/testimg.jpg" alt="asdf"/>
             </div>
         </div>
 
