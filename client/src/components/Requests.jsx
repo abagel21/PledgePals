e42ba7e97b6efbb4b0beb1104bd3adade26ced70
platbullet <50148871+platbullet@users.noreply.github.com>
2020-09-19 00:48:10 -0500

    requests

--- client/src/components/Requests.jsx
+++ client/src/components/Requests.jsx
@@ -3,11 +3,39 @@ import PropTypes from 'prop-types'
 import './styles/Requests.css'
 
 const Requests = props => {
+    const [reqArr, setReqArr] = React.useState([]);
+    
+    function handleClick(){
+        setReqArr(function(prev){
+            let img = "../images/medallion_img.png";
+            prev.push(new req(img, "asdf","1/2/2020", img));
+            return prev;
+        });
+        console.log(reqArr);
+    }
     return (
-        <div>
-            <h1>does this work?</h1>
+        <div className="requestswrapper">
+            <img src="../images/medallion_img.png" alt="medal"/>
+            <button onClick={handleClick}>Add Request</button>
+            {reqArr.map(function(e){
+                return (<div>
+                    <img src={e.image} alt="s"/>
+                    <h1>{e.name}</h1>
+                    <h1>{e.date}</h1>
+                    <h1>{e.medal}</h1>
+                </div>
+                )
+            })}
+            <ul>{reqArr}</ul>
         </div>
     )
+}
+
+function req(image,name, date, medal){
+    this.image = image;
+    this.name = name;
+    this.date = date;
+    this.medal = medal;  
 }
 
 Requests.propTypes = {
