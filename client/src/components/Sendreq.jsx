c8f60074d9b6d6f9db15ca6a258178100e7c3975
platbullet <50148871+platbullet@users.noreply.github.com>
2020-09-19 12:34:37 -0500

    sendreq

--- client/src/components/Sendreq.jsx
+++ client/src/components/Sendreq.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import './styles/Sendreq.css'
 import axios from 'axios'
 
 const Sendreq = props => {
-    function sendreq(){
-        axios.get();
+    const [nameInput, setNameInput] = React.useState("");
+    function sendreq(event){
+        event.preventDefault();
+        if(nameInput == ""){
+            return;
+        })
+        axios.post('/create/' + nameInput,{content:"sdf"},{headers: {"Content-Type":"application/json"}});
     }
+
+    function handleChange(event){
+        let x = event.target.value
+        setNameInput((prev) => {
+            return x;
+        });
+    }
+    useEffect(() =>{
+        axios.get("/api/auth").then(function(data){
+            console.log(data.data);
+        })
+    });
     return (
         <div className="sendreq-wrapper">
             <div>
-                <button className="btn btn-primary" onClick={sendreq}>Send a request</button>
+                <form>
+                    <input onChange={handleChange}></input>
+                    <button type="submit" className="btn btn-primary" onClick={sendreq}>Send a request</button>
+                </form>
+                <h1>Use this page to send requests to another user!</h1>
+
             </div>
         </div>
 
