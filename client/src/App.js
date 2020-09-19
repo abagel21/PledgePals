538c919f57e23cf92f0c5d6538d4fdb3cfabd8fd
Alex Nagel <slipshot21@gmail.com>
2020-09-18 22:22:42 -0500

    react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import logo from './logo.svg';
 import Login from "./components/Login.jsx";
 import Home from "./components/Home.jsx";
 import Navbar from "./components/navbar/Navbar"
 
+
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <Navbar />
-    </div>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </Fragment>
+    </Router>
   );
 }
 
