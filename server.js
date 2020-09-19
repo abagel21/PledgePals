3e2c3768b5d18effbdd3e720668c3b3254e41c77
Alex Nagel <slipshot21@gmail.com>
2020-09-19 11:56:12 -0500

    lightly tested friend routes

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const connectDB = require('./config/db');
 const User = require("./models/User");
 const users = require("./routes/auth")
 const medallions = require('./routes/medallion')
+const friends = require('./routes/friend')
 const { local } = require("./config/passport");
 
 connectDB();
@@ -63,6 +64,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use("/api/auth", users)
 app.use("/api/medallion", medallions)
+app.use("/api/friends", friends);
 
 let server = app.listen(PORT, () => {
     console.log("Server running on " + PORT);
