2c7adf75e143fb593b4f847874a513bf91a14492
Alex Nagel <slipshot21@gmail.com>
2020-09-19 00:48:04 -0500

    backend auth and login work

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
-import {Link} from 'react-router-dom'
+import axios from "axios"
 import "./styles/Auth.css"
 
 const Login = props => {
-    const onClick = (e) => {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    })
+    const onChange = (e) => {
+        console.log(formData.email);
+        setFormData({...formData, [e.target.name] : e.target.value});
+    }
+    const onSubmit = (e) => {
         e.preventDefault();
+        axios.get("/api/auth/login")
     }
     return (
-        <div className="loginWrapper">
+        <div className="loginWrapper" onSubmit = {e => onSubmit(e)}>
             <div className="formsContainer">
                 <form action="" className="loginForm">
                     <h2 className="heading">Login</h2>
                     <div className="loginEmail">
-                        <input type="text" className="loginField username" required/>
+                        <input type="text" className="loginField username" onChange = {e =>onChange(e)} value = {formData.email} name="email" required/>
                         <label>Email Address</label>
                     </div>
                     <div className="loginPassword">
-                        <input type="password" className="loginField password" required/>
+                        <input type="password" className="loginField password" onChange = {e =>onChange(e)} value = {formData.password} name="password"  required/>
                         <label>Password</label>
                     </div>
-                    <Link to='/dashboard' className="loginSubmit">Log In</Link>
+                    <input type="submit" className="loginSubmit">Log In</input>
                 </form>
             </div>
         </div>
