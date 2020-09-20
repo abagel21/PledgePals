73187fe27779e6ab90478df8dadeb4bc0a21d040
Alex Nagel <slipshot21@gmail.com>
2020-09-20 00:10:30 -0500

    build

---
+++ client/build/precache-manifest.7cef7451f67253b0b15d1c5e3a608c84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10289cc007fd857521b9bb9d96dbebd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc430edfa926558c5fa3",
+    "url": "/static/css/main.35b3315a.chunk.css"
+  },
+  {
+    "revision": "1c7dde74d59ddebd9841",
+    "url": "/static/js/2.c777f3c3.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "/static/js/2.c777f3c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc430edfa926558c5fa3",
+    "url": "/static/js/main.0de55543.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9fe01bc66a9b48fab4ed419c7fef5a27",
+    "url": "/static/media/pfp_1.9fe01bc6.png"
+  }
+]);
